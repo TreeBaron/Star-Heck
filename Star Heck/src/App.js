8bd0568901ef17a5e71eb86bae9ea1d4fe9c2dde
John Dodd <harbingerofvengeance@gmail.com>
2023-07-29 15:12:06 -0700

    Added Typo Generation Code

--- Star Heck/src/App.js
+++ Star Heck/src/App.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 import { Button } from 'react-bootstrap';
 import Cookies from 'universal-cookie';
+import { getTypos } from './CommandParser';
 
 function App() {
+
+  const [typos, setTypos] = useState(null);
 
   function handleSubmit(e) {
     // Prevent the browser from reloading the page
@@ -16,9 +19,15 @@ function App() {
   let statusColor = 'red';
   let status = 'yellow';
 
+  if(!typos) {
+    //setTypos(getTypos('ravioli').toString());
+    setTypos(getTypos('ravioli')[0]);
+  }
+
+
   return (
     <div className='everything'>
-    <textarea value={'He fires the phasers.'} readOnly={true} onChange={() => console.log('change')} className="textFeed globalCentering"/>
+    <textarea value={typos} readOnly={true} onChange={() => console.log('change')} className="textFeed globalCentering"/>
     <div className='statusDisplay globalCentering'>
       <span class={status === 'red' ? "red dot" : status === 'yellow' ?  "yellow dot" : "green dot"} /> {statusText}
     </div>
--- Star Heck/src/CommandParser.js
+++ Star Heck/src/CommandParser.js
@@ -31,10 +31,59 @@ carry (body)
 use (item)
 */
 
-getTypos(input) {
-    for(let i = 0; i < input.length; i++)
-    {
-        let letter = input.charAt(i);
-        
+export function getTypos(str) {
+ 
+    const replaceAt = (text, index, char) => {
+	    return text.substr(0, index) + char + text.substr(index+char.length);
     }
+
+    //define proximity arrays
+    var array_prox = [];
+    array_prox['a'] = ['q', 'w', 'z', 'x'];
+    array_prox['b'] = ['v', 'f', 'g', 'h', 'n'];
+    array_prox['c'] = ['x', 's', 'd', 'f', 'v'];
+    array_prox['d'] = ['x', 's', 'w', 'e', 'r', 'f', 'v', 'c'];
+    array_prox['e'] = ['w', 's', 'd', 'f', 'r'];
+    array_prox['f'] = ['c', 'd', 'e', 'r', 't', 'g', 'b', 'v'];
+    array_prox['g'] = ['r', 'f', 'v', 't', 'b', 'y', 'h', 'n'];
+    array_prox['h'] = ['b', 'g', 't', 'y', 'u', 'j', 'm', 'n'];
+    array_prox['i'] = ['u', 'j', 'k', 'l', 'o'];
+    array_prox['j'] = ['n', 'h', 'y', 'u', 'i', 'k', 'm'];
+    array_prox['k'] = ['u', 'j', 'm', 'l', 'o'];
+    array_prox['l'] = ['p', 'o', 'i', 'k', 'm'];
+    array_prox['m'] = ['n', 'h', 'j', 'k', 'l'];
+    array_prox['n'] = ['b', 'g', 'h', 'j', 'm'];
+    array_prox['o'] = ['i', 'k', 'l', 'p'];
+    array_prox['p'] = ['o', 'l'];
+    array_prox['r'] = ['e', 'd', 'f', 'g', 't'];
+    array_prox['s'] = ['q', 'w', 'e', 'z', 'x', 'c'];
+    array_prox['t'] = ['r', 'f', 'g', 'h', 'y'];
+    array_prox['u'] = ['y', 'h', 'j', 'k', 'i'];
+    array_prox['v'] = ['', 'c', 'd', 'f', 'g', 'b'];    
+    array_prox['w'] = ['q', 'a', 's', 'd', 'e'];
+    array_prox['x'] = ['z', 'a', 's', 'd', 'c'];
+    array_prox['y'] = ['t', 'g', 'h', 'j', 'u'];
+    array_prox['z'] = ['x', 's', 'a'];
+    array_prox['1'] = ['q', 'w'];
+    array_prox['2'] = ['q', 'w', 'e'];
+    array_prox['3'] = ['w', 'e', 'r'];
+    array_prox['4'] = ['e', 'r', 't'];
+    array_prox['5'] = ['r', 't', 'y'];
+    array_prox['6'] = ['t', 'y', 'u'];
+    array_prox['7'] = ['y', 'u', 'i'];
+    array_prox['8'] = ['u', 'i', 'o'];
+    array_prox['9'] = ['i', 'o', 'p'];
+    array_prox['0'] = ['o', 'p'];
+
+    let arr = [];
+	for(let a=0; a<str.length; a++) {
+	    let temp = array_prox[str.charAt(a)];    
+		for(let b=0; b<temp.length; b++) {
+		    let typo = replaceAt(str, a, temp[b]);
+            console.log(typo);
+		    arr.push(typo);
+		}
+	}
+
+	return arr;
 }
